9bf2cebc2428b166c238c8c5e56dac035869c3e9
Anushree Kulai <anushreekulai@Anushrees-MacBook-Air.local>
2023-01-19 23:48:04 -0500

    testing 5 datasets

--- anushree_experiments.py
+++ anushree_experiments.py
@@ -33,16 +33,16 @@ DATASETS = {
     "Abalone": "Rings",
     # "bank8FM": "rej",
     # "heat": "heat",
-    # "cpuSm": "usr",
-    # "energy": "Appliances",
+    "cpuSm": "usr",
+    "energy": "Appliances",
     # "superconductivity": "critical_temp",
     # "creditScoring": "NumberOfDependents",
     # "census": "Unemployment",
     # "onion_prices": "modal_price",
     # "delays": "perdelay",
     # "gpu_performance": "Run4(ms)",
-    # "CountyHousing": "Total_Sale_Price",
-    # "HousePrices": "Prices",
+    "CountyHousing": "Total_Sale_Price",
+    "HousePrices": "Prices",
     # "salary_compensation": "TotalCompensation",
 }
 
@@ -146,124 +146,137 @@ REGRESSORS = {
 spark = SparkSession.builder.master('local[4]').appName('Distributed Resampling').getOrCreate()
 
 execution_times = {}
+results = {}
 
 # adding debugging print statement
 
 # adding exception handling for all the sampling techniques
 
-for dataset, label_col in DATASETS.items():
+# Running for 10 iterations
 
-    print(dataset)
-    DATA_PROCESSED_TRAIN_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/train"
-    DATA_PROCESSED_TEST_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/test"
-
-    print("Reading DF")
-    df = pd.read_csv(f"{DATA_RAW_DIR}/{dataset}.csv")
-
-    print("Reading DF in spark!")
-    df = spark.createDataFrame(df)
-
-    print("Calculating Phi value")
-    relevance_col = "phi"
-    df = Phi(input_col=label_col, output_col=relevance_col).transform(df)
-
-    execution_times[dataset] = {}
-    results = {}
-
-    print("Splitting and Pre-processing")
-    X = df.toPandas()
-    y = X[label_col]
-    k_fold_obj = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-    k_fold_splits = k_fold_obj.split(X,y)
-
-    for i, (train_indices, test_indices) in enumerate(k_fold_splits):
-        print(f"Fold {i}:")
-
-        X_train = X.iloc[train_indices]
-        X_test = X.iloc[test_indices]
+for iteration in range(10):
 
 
-        # Remove index column
-        X_train.drop(columns=[relevance_col], inplace=True)
-        if "Unnamed: 0" in X_train.columns:
-            X_train.drop(columns=["Unnamed: 0"], inplace=True)
-        if "Unnamed: 0" in X_test.columns:
-            X_test.drop(columns=["Unnamed: 0"], inplace=True)
+    for dataset, label_col in DATASETS.items():
 
-        train = spark.createDataFrame(pd.DataFrame(X_train))
-        print(f"{X_train.shape}, {X_test.shape}")
+        print(dataset)
+        DATA_PROCESSED_TRAIN_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/train"
+        DATA_PROCESSED_TEST_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/test"
 
-        phi = X_test.pop(relevance_col)
+        print("Reading DF")
+        df = pd.read_csv(f"{DATA_RAW_DIR}/{dataset}.csv")
 
-        print("Saving the CSV files")
-        X_test.to_csv(f"{DATA_PROCESSED_TEST_DIR}/{dataset}_Fold_{i}.csv", index=False)
-        phi.to_csv(f"{DATA_PROCESSED_TEST_DIR}/{dataset}_phi.csv", index=False)
-        X_train.to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_Fold_{i}.csv", index=False)
+        print("Reading DF in spark!")
+        df = spark.createDataFrame(df)
 
-        try:
-            print("Initializing Distributed RUS")
-            start_time = time.time()
-            train_rus = DistributedRUS(label_col=label_col, k_partitions=1).transform(train)
-            end_time = time.time()
-            execution_times[dataset]["RUS_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_rus.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_rus_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Exception found in Distributed RUS: {e}")
+        print("Calculating Phi value")
+        relevance_col = "phi"
+        df = Phi(input_col=label_col, output_col=relevance_col).transform(df)
 
-        try:
-            print("Initializing Distributed ROS")
-            start_time = time.time()
-            train_ros = DistributedROS(label_col=label_col, k_partitions=1).transform(train)
-            end_time = time.time()
-            execution_times[dataset]["ROS_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_ros.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_ros_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Exception found in Distributed ROS: {e}")
+        print("Splitting and Pre-processing")
+        X = df.toPandas()
+        y = X[label_col]
+        k_fold_obj = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-        try:
-            print("Initializing SMOGN")
-            start_time = time.time()
-            train_smogn = smoter(data=train.toPandas(), y=label_col)
-            end_time = time.time()
-            execution_times[dataset]["SMOGN_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_smogn.to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_smogn_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Exception found in SMOGN: {e}")
+        k_fold_splits = k_fold_obj.split(X,y)
 
-        try:
-            print("Initializing Distributed SMOGN with 2 partitions!")
-            start_time = time.time()
-            train_dist_smogn_2 = DistributedSMOGN(label_col=label_col, k_partitions=2).transform(train)
-            end_time = time.time()
-            execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_dist_smogn_2.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_2_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Found exception in DistSMOGN-2P: {e}")
+        for i, (train_indices, test_indices) in enumerate(k_fold_splits):
+            print(f"Fold {i}:")
 
-        try:
-            print("Initializing Distributed SMOGN with 4 partitions!")
-            start_time = time.time()
-            train_dist_smogn_4 = DistributedSMOGN(label_col=label_col, k_partitions=4).transform(train)
-            end_time = time.time()
-            execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_dist_smogn_4.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_4_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Found exception in DistSMOGN-4P: {e}")
-
-        try:
-            print("Initializing Distributed SMOGN with 8 partitions!")
-            start_time = time.time()
-            train_dist_smogn_8 = DistributedSMOGN(label_col=label_col, k_partitions=8).transform(train)
-            end_time = time.time()
-            execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold "+str(i)] = round(end_time - start_time, 3)
-            train_dist_smogn_8.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_8_Fold_{i}.csv", index=False)
-        except Exception as e:
-            print(f"Found exception in Dist-SMOGN-8P: {e}")
+            X_train = X.iloc[train_indices]
+            X_test = X.iloc[test_indices]
 
 
-        # Experiments to observe Performance on different regressors
-        for dataset, label_col in DATASETS.items():
+            # Remove index column
+            X_train.drop(columns=[relevance_col], inplace=True)
+            if "Unnamed: 0" in X_train.columns:
+                X_train.drop(columns=["Unnamed: 0"], inplace=True)
+            if "Unnamed: 0" in X_test.columns:
+                X_test.drop(columns=["Unnamed: 0"], inplace=True)
+
+            train = spark.createDataFrame(pd.DataFrame(X_train))
+            print(f"{X_train.shape}, {X_test.shape}")
+
+            phi = X_test.pop(relevance_col)
+
+            print("Saving the CSV files")
+            X_test.to_csv(f"{DATA_PROCESSED_TEST_DIR}/{dataset}_Fold_{i}.csv", index=False)
+            phi.to_csv(f"{DATA_PROCESSED_TEST_DIR}/{dataset}_phi.csv", index=False)
+            X_train.to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_Fold_{i}.csv", index=False)
+
+            if iteration == 0:
+                if i == 0:
+                    execution_times[dataset] = {}
+                execution_times[dataset]["RUS_Fold " + str(i)] = 0
+                execution_times[dataset]["ROS_Fold " + str(i)] = 0
+                execution_times[dataset]["SMOGN_Fold " + str(i)] = 0
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold " + str(i)] = 0
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold " + str(i)] = 0
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold " + str(i)] = 0
+
+            try:
+                print("Initializing Distributed RUS")
+                start_time = time.time()
+                train_rus = DistributedRUS(label_col=label_col, k_partitions=1).transform(train)
+                end_time = time.time()
+                execution_times[dataset]["RUS_Fold "+str(i)] = execution_times[dataset]["RUS_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_rus.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_rus_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Exception found in Distributed RUS: {e}")
+
+            try:
+                print("Initializing Distributed ROS")
+                start_time = time.time()
+                train_ros = DistributedROS(label_col=label_col, k_partitions=1).transform(train)
+                end_time = time.time()
+                execution_times[dataset]["ROS_Fold "+str(i)] = execution_times[dataset]["ROS_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_ros.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_ros_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Exception found in Distributed ROS: {e}")
+
+            try:
+                print("Initializing SMOGN")
+                start_time = time.time()
+                train_smogn = smoter(data=train.toPandas(), y=label_col)
+                end_time = time.time()
+                execution_times[dataset]["SMOGN_Fold "+str(i)] = execution_times[dataset]["SMOGN_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_smogn.to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_smogn_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Exception found in SMOGN: {e}")
+
+            try:
+                print("Initializing Distributed SMOGN with 2 partitions!")
+                start_time = time.time()
+                train_dist_smogn_2 = DistributedSMOGN(label_col=label_col, k_partitions=2).transform(train)
+                end_time = time.time()
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold "+str(i)] = execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_dist_smogn_2.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_2_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Found exception in DistSMOGN-2P: {e}")
+
+            try:
+                print("Initializing Distributed SMOGN with 4 partitions!")
+                start_time = time.time()
+                train_dist_smogn_4 = DistributedSMOGN(label_col=label_col, k_partitions=4).transform(train)
+                end_time = time.time()
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold "+str(i)] = execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_dist_smogn_4.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_4_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Found exception in DistSMOGN-4P: {e}")
+
+            try:
+                print("Initializing Distributed SMOGN with 8 partitions!")
+                start_time = time.time()
+                train_dist_smogn_8 = DistributedSMOGN(label_col=label_col, k_partitions=8).transform(train)
+                end_time = time.time()
+                execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold "+str(i)] = execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold "+str(i)] + round(end_time - start_time, 3)
+                train_dist_smogn_8.toPandas().to_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}_dist_smogn_8_Fold_{i}.csv", index=False)
+            except Exception as e:
+                print(f"Found exception in Dist-SMOGN-8P: {e}")
+
+
+            # Experiments to observe Performance on different regressors
+            # for dataset, label_col in DATASETS.items():
             DATA_PROCESSED_TRAIN_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/train"
             DATA_PROCESSED_TEST_DIR = f"{DATA_PROCESSED_DIR}/{dataset}/test"
 
@@ -271,6 +284,7 @@ for dataset, label_col in DATASETS.items():
 
                 print(dataset + " " + regressor + "###############################################")
                 for experiment, experiment_config in EXPERIMENTS_PERFORMANCE.items():
+                    print("------- Running EXPT loop again ------")
                     train = pd.read_csv(f"{DATA_PROCESSED_TRAIN_DIR}/{dataset}{experiment_config['file_postfix']}_Fold_{i}.csv")
                     y_train = train.pop(label_col)
                     x_train = pd.get_dummies(train)
@@ -287,7 +301,8 @@ for dataset, label_col in DATASETS.items():
 
                     # x_train, x_test = x_train.align(x_test, join='outer', axis=1)  # outer join --RATHER ONE HOT ENCODER BEFORE FITTING
                     print("reading datasets done")
-                    scaler = MinMaxScaler().fit(pd.concat([x_train, x_test]))
+                    # Only fitting the MinMaxScalar on train and later transforming both train and test.
+                    scaler = MinMaxScaler().fit(x_train)
 
 
                     x_train = scaler.transform(x_train)
@@ -313,7 +328,14 @@ for dataset, label_col in DATASETS.items():
                     results[experiment_config["name"]+"_Fold_"+str(i)]["mae"] = round(np.mean(mae_list), 3)
                     results[experiment_config["name"]+"_Fold_"+str(i)]["rmse"] = round(np.mean(rmse_list), 3)
 
-                pd.DataFrame(data=results).transpose().to_csv(f"{RESULT_PREDICTIVE_PERFORMANCE_DIR}/{dataset}/{regressor}.csv",index=True)
-                print("Results noted")
+
+    for folds in range(5):
+        execution_times[dataset]["RUS_Fold " + str(i)] = (execution_times[dataset]["RUS_Fold "+str(i)])/10
+        execution_times[dataset]["ROS_Fold " + str(i)] = (execution_times[dataset]["ROS_Fold "+str(i)])/10
+        execution_times[dataset]["SMOGN_Fold " + str(i)] = (execution_times[dataset]["SMOGN_Fold "+str(i)])/10
+        execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold " + str(i)] = (execution_times[dataset]["Distributed SMOGN (k_partitions = 2)_Fold "+str(i)])/10
+        execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold " + str(i)] = (execution_times[dataset]["Distributed SMOGN (k_partitions = 4)_Fold "+str(i)])/10
+        execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold " + str(i)] = (execution_times[dataset]["Distributed SMOGN (k_partitions = 8)_Fold "+str(i)])/10
 
 pd.DataFrame(data=execution_times).to_csv(f"{RESULT_EXECUTION_TIME_DIR}/execution_time.csv", index=True)
+pd.DataFrame(data=results).transpose().to_csv(f"{RESULT_PREDICTIVE_PERFORMANCE_DIR}/{dataset}/{regressor}.csv",index=True)
