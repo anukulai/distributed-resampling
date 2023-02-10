ef4f9545881c0dc3b8cd156e0c2d774a72b4af77
Anushree Kulai <anushreekulai@Anushrees-MacBook-Air.local>
2023-02-10 02:21:43 -0500

    added comments

--- src/sampling/mixed_sampling/distributed_smogn.py
+++ src/sampling/mixed_sampling/distributed_smogn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import random
 from pyspark import keyword_only
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.feature import VectorAssembler
@@ -9,6 +10,20 @@ from src.params.sampling._smogn import _KMeansParams, _SMOGNParams
 from src.sampling.mixed_sampling.base import BaseMixedSampler
 from src.utils.dataframe import get_num_cols, get_cat_cols
 
+
+def is_single_partition_present(df):
+    """
+    This function checks for presence of single valued partitions.
+    :param df: dataframe with partitions
+    :return: True if the there is a presence of a partition with 1 count.
+    """
+    candidacy_dict = df.partition.value_counts().to_dict()
+
+    for partition_key, candidacy in candidacy_dict.items():
+        if candidacy == 1:
+            return True
+
+    return False
 
 class DistributedSMOGN(BaseMixedSampler, _KMeansParams, _SMOGNParams):
     @keyword_only
@@ -30,13 +45,21 @@ class DistributedSMOGN(BaseMixedSampler, _KMeansParams, _SMOGNParams):
         feature_vector_col = "feature_vector"
         feature_vector_cols = get_num_cols(df)
 
-        df = VectorAssembler(inputCols=feature_vector_cols, outputCol=feature_vector_col).transform(df)
-        df = KMeans(featuresCol=feature_vector_col, predictionCol=partition_col, k=self.getKPartitions(),
-                    initSteps=self.getInitSteps(), tol=self.getTol(), maxIter=self.getMaxIter()).fit(df).transform(df)
+        redo_partition = True
 
-        df = df.drop(feature_vector_col)
+        while redo_partition:
+            df_new = df
+            df_new = VectorAssembler(inputCols=feature_vector_cols, outputCol=feature_vector_col).transform(df_new)
+            df_new = KMeans(
+                featuresCol=feature_vector_col, predictionCol=partition_col, k=self.getKPartitions(),
+                initSteps=self.getInitSteps(), tol=self.getTol(), maxIter=self.getMaxIter(),
+                seed=random.randint(0, 1000)
+            ).fit(df_new).transform(df_new)
 
-        return df.repartition(self.getKPartitions(), partition_col)
+            df_new = df_new.drop(feature_vector_col)
+            redo_partition = is_single_partition_present(df_new.toPandas())
+
+        return df_new.repartition(self.getKPartitions(), partition_col)
 
     def _oversample(self, bump):
         schema = bump.samples.schema
@@ -68,6 +91,7 @@ class DistributedSMOGN(BaseMixedSampler, _KMeansParams, _SMOGNParams):
             return pd.DataFrame(data=synth_samples)
 
         return bump.samples.groupby(partition_col).applyInPandas(create_synth_samples, schema=schema)
+        # return bump.samples.toPandas().groupby(partition_col).apply(create_synth_samples)
 
     def _undersample(self, bump):
         return super()._partition(bump.samples).sample(withReplacement=False, fraction=bump.sampling_percentage)
@@ -125,24 +149,38 @@ class DistributedSMOGN(BaseMixedSampler, _KMeansParams, _SMOGNParams):
         if(n_rows>1):
             k = min(k, n_rows)
 
+        # Calculate feature vectors
         feature_vectors = partition[[*num_feature_cols]].to_numpy()
+
+        # Calculate euclidean distances between various combination of rows.
         dist_matrix = euclidean_distances(feature_vectors, feature_vectors)
+
+        # This is an n_rows by k matrix. Each row represents a point with indices to its k nearest neighbours.
         neighbour_sample_index_matrix = np.delete(np.argsort(dist_matrix, axis=1), np.s_[k + 1:], axis=1)
 
+        # a dictionary representing the distribution of categorical features
         cat_feature_probs = {
             cat_feature_col: partition[cat_feature_col].value_counts(normalize=True).to_dict()
             for cat_feature_col in cat_feature_cols
         }
+
+        # standard deviations of all the numerical features in partition
         num_feature_stds = partition[[*num_feature_cols]].std()
+
+        # standard deviation of the target column
         label_std = partition[label_col].std()
 
+        # placeholder for the number of synthetic samples ---> n_rows are incremented by a factor of n_synth_samples
         synth_samples = [None for _ in range(n_rows * n_synth_samples)]
 
         for base_sample_index, base_sample in partition.iterrows():
+            # Iterate through the partition and fetch distances and neighbour indices for a particular row.
             dists = dist_matrix[base_sample_index]
             neighbour_sample_indices = neighbour_sample_index_matrix[base_sample_index]
 
             for n_synth_sample in range(n_synth_samples):
+
+                # pick any random neighbour index
                 neighbour_sample_index = np.random.choice(neighbour_sample_index_matrix[base_sample_index])
                 neighbour_sample = partition.iloc[neighbour_sample_index]
 
